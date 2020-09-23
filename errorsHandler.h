16d54d4339a977702360ef12ffabef7622be297f
Ховалыг Тараа <hovalygtaraa@Hovalygs-MacBook-Air.local>
2020-09-23 19:19:53 +0300

    1

---
+++ errorsHandler.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errorsHandler.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hovalygtaraa <marvin@42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/23 17:20:58 by hovalygta         #+#    #+#             */
+/*   Updated: 2020/09/23 19:01:25 by hovalygta        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERRORSHANDLER_H
+# define ERRORSHANDLER_H
+# define WRONG_ARGC 0
+# include "structures.h"
+
+void	errorsHandler(int error_id, t_scene *scene);
+
+#endif
